bf2d2bd3bace87d95ee395ec5e7024e720d77d3a
Rafaela Carvalho <rafaela.a.dos.santos@avanade.com>
2022-10-13 16:15:31 -0300

    Deploy

--- pages/extract.tsx
+++ pages/extract.tsx
@@ -1,4 +1,33 @@
 import React from 'react';
+import {
+    Typography,
+    Container,
+    Button,
+    Avatar,
+    TextField,
+    Checkbox,
+    Grid,
+    Box,
+    CssBaseline} from '@mui/material';
+import Link from 'next/link';    
+//@mui/material 
+
+type CopyProps = {
+    site: string;
+}
+function Copyright(props: CopyProps){
+    return(
+        <Typography>
+            {'Copyright ©️'}
+            <Link color="inherit" href={`https://www.${props.site}.com.br`}>
+            {props.site}
+            </Link>
+            {' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    )
+}
 
 export default function ExtractPage() {
   return (
